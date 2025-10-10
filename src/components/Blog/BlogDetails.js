9dc54b5a55bf5bc6262ee9cf5585e85278c3b792
Ozkan Cimenli <ozkan.cimenli@hotmail.com>
2025-10-10 19:41:18 -0400

    again

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -7,25 +7,27 @@ import SocialShare from "./SocialShare";
 
 const BlogDetails = ({ blog, slug: blogSlug }) => {
   return (
-    <div className="px-2  md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5  md:mx-10 rounded-lg">
-      <time className="m-3">
-        {format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
-      </time>
-      <span className="m-3">
-        <ViewCounter slug={blogSlug} />
-      </span>
-      <div className="m-3">{blog.readingTime.text}</div>
-      <Link href={`/categories/${slug(blog.tags[0])}`} className="m-3">
-        #{blog.tags[0]}
-      </Link>
-    </div>
-    <div className="px-2 md:px-10 mx-5 md:mx-10 mt-4">
-      <SocialShare 
-        title={blog.title}
-        url={`/blogs/${blogSlug}`}
-        description={blog.description}
-      />
-    </div>
+    <>
+      <div className="px-2  md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5  md:mx-10 rounded-lg">
+        <time className="m-3">
+          {format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
+        </time>
+        <span className="m-3">
+          <ViewCounter slug={blogSlug} />
+        </span>
+        <div className="m-3">{blog.readingTime.text}</div>
+        <Link href={`/categories/${slug(blog.tags[0])}`} className="m-3">
+          #{blog.tags[0]}
+        </Link>
+      </div>
+      <div className="px-2 md:px-10 mx-5 md:mx-10 mt-4">
+        <SocialShare 
+          title={blog.title}
+          url={`/blogs/${blogSlug}`}
+          description={blog.description}
+        />
+      </div>
+    </>
   );
 };
 
