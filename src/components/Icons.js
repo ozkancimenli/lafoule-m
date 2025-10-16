c208f55dca181d9cf22b5859056351b629240fa9
Ozkan Cimenli <ozkan.cimenli@hotmail.com>
2025-10-16 12:10:41 -0400

    UI: replace Twitter bird with X logo in Icons.TwitterIcon

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -365,26 +365,20 @@ export const LinkedinIcon = ({ className, ...rest }) => {
 };
 
 export const TwitterIcon = ({ className, ...rest }) => {
+  // Render the X (formerly Twitter) logo while keeping the same export name
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
       width={42}
       height={43}
-      viewBox='0 0 42 43'
+      viewBox='0 0 24 24'
       className={cx('w-full h-auto', className)}
       {...rest}
     >
-      <g clipPath='url(#a)'>
-        <path
-          fill='#1D9BF0'
-          d='M37.7 13.24c.026.37.026.74.026 1.114 0 11.386-8.668 24.517-24.518 24.517v-.006A24.394 24.394 0 0 1 0 35.002a17.303 17.303 0 0 0 12.753-3.571 8.627 8.627 0 0 1-8.05-5.984 8.584 8.584 0 0 0 3.89-.149 8.617 8.617 0 0 1-6.912-8.446v-.11a8.562 8.562 0 0 0 3.91 1.08A8.627 8.627 0 0 1 2.926 6.315a24.457 24.457 0 0 0 17.758 9.002 8.622 8.622 0 0 1 2.494-8.233 8.626 8.626 0 0 1 12.19.374 17.29 17.29 0 0 0 5.473-2.092 8.649 8.649 0 0 1-3.788 4.766A17.135 17.135 0 0 0 42 8.776a17.504 17.504 0 0 1-4.3 4.464Z'
-        />
-      </g>
-      <defs>
-        <clipPath id='a'>
-          <path fill='#fff' d='M0 .808h42v42H0z' />
-        </clipPath>
-      </defs>
+      <path
+        fill='currentColor'
+        d='M3 3h3.3l5.2 6.9L17.7 3H21l-7.4 9.8L21.6 21H18.3l-5.6-7.4L6.1 21H2.8l7.9-10.3L3 3Z'
+      />
     </svg>
   );
 };
